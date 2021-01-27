4186dda04611fd9fadb00f593f4d1c085884264b
kafi <abdullahkafi996@gmail.com>
2021-01-28 00:52:40 +0600

    add calculation method for deposit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// login event handler
 var loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+})
+// deposit event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    // console.log(totalDeposit);
+
+    // add deposit amount
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+    document.getElementById("depositAmount").value = "";
+
 })
