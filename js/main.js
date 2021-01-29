0a1a7f524d98b339fa3ba4f4cd940f6b0a7d90af
kafi <abdullahkafi996@gmail.com>
2021-01-29 15:23:44 +0600

    add withdraw calculation with main balance and complete this project

--- js/main.js
+++ js/main.js
@@ -42,7 +42,10 @@ depositBtn.addEventListener("click", function () {
 const withdrawBtn = document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click", function () {
    const withdrawNumber = getInputNumber("withdrawAmount");
-   console.log(withdrawNumber);
+   updateSpanText("currentWithdraw", withdrawNumber);
+   updateSpanText("currentBalance", -1* withdrawNumber );
+
+   document.getElementById("withdrawAmount").value = "";
 })
 
 function getInputNumber(id) {
